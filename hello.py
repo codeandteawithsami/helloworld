ba5c5fe6919caeef038559a3966e9519a421cfaa
Code and Tea With Samiullah <142134499+codeandteawithsami@users.noreply.github.com>
2024-10-18 11:52:56 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-input = input("Enter Your value!")
-if input == "sami":
-  print("Hello from Samiullah!")
+try:
+    user_input = input("Enter Your value!")
+except EOFError:
+    user_input = "default"  # Use a default value if no input is provided
+
+if user_input == "sami":
+    print("Hello from Samiullah!")
 else:
-  for i in range(7):
-    print("Samiullah is a good boy")
-  
+    for i in range(7):
+        print("Samiullah is a good boy")
