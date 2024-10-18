b8692a7ca6b613826ba01ba1ad9eaef04376b209
Code and Tea With Samiullah <142134499+codeandteawithsami@users.noreply.github.com>
2024-10-18 12:03:28 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-try:
-    user_input = input("Enter Your value!")
-except EOFError:
-    user_input = "default"  # Use a default value if no input is provided
+import os
+
+# Get the input from the environment variable
+user_input = os.getenv("USER_INPUT", "default")  # Default value if not set
 
 if user_input == "sami":
     print("Hello from Samiullah!")
