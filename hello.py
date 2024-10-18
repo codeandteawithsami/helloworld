5992b812a5dc561c4707f6ba4353486fdd605b0c
Code and Tea With Samiullah <142134499+codeandteawithsami@users.noreply.github.com>
2024-10-18 12:31:26 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ import os
 
 # Get the input from the environment variable
 user_input = os.getenv("USER_INPUT", "default")  # Default value if not set
+user_input1 = os.getenv("USER_INPUT", "default")
 
-if user_input == "sami":
+if user_input == user_input1:
     print("Hello from Samiullah!")
 else:
     for i in range(7):
